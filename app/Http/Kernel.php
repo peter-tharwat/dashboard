5c5c993ddb5e4d7d7327c7f94d6a1486f075c1f9
peter-tharwat <ptharwat@gmail.com>
2022-05-02 23:53:51 +0200

    m

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+        ]
     ];
 
     /**
@@ -57,6 +57,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+         'cors' => \Barryvdh\Cors\Middleware\HandleCors::class,
+
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
